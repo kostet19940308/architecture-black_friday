601cd62af2861ffcae46260fa9559ceab49e1c84
Konstantin podobryanskiy <kostet19940308@gmail.com>
2025-12-17 00:12:02 +0300

    Task 3, 4, 5, 6

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -13,22 +13,26 @@ rs.initiate({
 })
 EOF
 
-echo "инициализация shard1"
-docker compose exec -T shard1 mongosh --port 27020 --quiet <<EOF
+echo "инициализация shard1 replica set"
+docker compose exec -T shard1_1 mongosh --port 27020 --quiet <<EOF
 rs.initiate({
     _id: "shard1",
     members: [
-        { _id: 0, host: "shard1:27020" }
+        { _id: 0, host: "shard1_1:27020", priority: 5 },
+        { _id: 1, host: "shard1_2:27020", priority: 3 },
+        { _id: 2, host: "shard1_3:27020", priority: 1 }
     ]
 })
 EOF
 
-echo "инициализация shard2"
-docker compose exec -T shard2 mongosh --port 27021 --quiet <<EOF
+echo "инициализация shard2 replica set"
+docker compose exec -T shard2_1 mongosh --port 27021 --quiet <<EOF
 rs.initiate({
     _id: "shard2",
     members: [
-        { _id: 0, host: "shard2:27021" }
+        { _id: 0, host: "shard2_1:27021", priority: 5 },
+        { _id: 1, host: "shard2_2:27021", priority: 3 },
+        { _id: 2, host: "shard2_3:27021", priority: 1 }
     ]
 })
 EOF
@@ -37,15 +41,15 @@ sleep 10
 
 echo "инициализация mongos_router и добавление данных"
 docker compose exec -T mongos_router mongosh --port 27017 --quiet <<EOF
-sh.addShard("shard1/shard1:27020")
-sh.addShard("shard2/shard2:27021")
+sh.addShard("shard1/shard1_1:27020,shard1_2:27020,shard1_3:27020")
+sh.addShard("shard2/shard2_1:27021,shard2_2:27021,shard2_3:27021")
 sh.enableSharding("somedb")
 
 use somedb
 db.helloDoc.createIndex({name:1})
 sh.shardCollection("somedb.helloDoc", { name: "hashed"})
 
-for(var i = 0; i < 2000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+for(var i = 0; i < 6000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
 sleep 5
@@ -54,12 +58,12 @@ echo "Количество документов на shard1:"
 docker compose exec -T shard1_1 mongosh --port 27020 --quiet <<EOF
 use somedb
 var shard1Count = db.helloDoc.countDocuments()
-print("Документов на shard1:", shard1Count)
+print("Документов на shard1rs:", shard1Count)
 EOF
 
 echo "Количество документов на shard2:"
 docker compose exec -T shard2_1 mongosh --port 27021 --quiet <<EOF
 use somedb
 var shard2Count = db.helloDoc.countDocuments()
-print("Документов на shard2:", shard2Count)
+print("Документов на shard2rs:", shard2Count)
 EOF
